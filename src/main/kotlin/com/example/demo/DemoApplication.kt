bd9982ac20e565cef2ebc1175b5f5e02c3ee0217
taichiii <aogaku415@gmail.com>
2024-02-05 11:40:45 +0900

    デモ完了

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,10 +2,54 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import java.util.Optional
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class MessageController(val service: MessageService) {
+    @GetMapping("/")
+    fun index(): List<Message> = service.findMessages()
+
+    @GetMapping("/{id}")
+    fun index(@PathVariable id: String): List<Message> =
+        service.findMessageById(id)
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
+}
+
+@Table("MESSAGES")
+data class Message(@Id var id: String?, val text: String)
+
+interface MessageRepository : CrudRepository<Message, String>
+
+@Service
+class MessageService(val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findAll().toList()
+
+    fun findMessageById(id: String): List<Message> = db.findById(id).toList()
+
+    fun save(message: Message) {
+        db.save(message)
+    }
+
+    fun <T : Any> Optional<out T>.toList(): List<T> =
+        if (isPresent) listOf(get()) else emptyList()
 }
